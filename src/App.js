3c5b77be6bb978ecdbc09377d97a8acc1069d3a5
jamatov <mirabdullajamatov@gmail.com>
2023-01-04 13:58:37 +0500

    1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+
 import Navigation from './components/Navigation';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,28 +3,20 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
+import Logo from '../images/logo.svg'
 
 export default function Navigation() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar className='nav_bar' collapseOnSelect expand="lg" variant="light">
+      <Container className='nav'>
+        <div>
+          <img src={Logo} alt="" />
+        </div>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            
           </Nav>
           <Nav>
             <Nav.Link href="#deets">More deets</Nav.Link>
