80ac18e4672d8f148482236a40ec3f9aa1136504
jamatov <mirabdullajamatov@gmail.com>
2023-01-05 15:40:21 +0500

    home search done

--- src/components/Home/HomeSearch.jsx
+++ src/components/Home/HomeSearch.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import { FaSearch } from 'react-icons/fa'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 export default function HomeSearch() {
   return (
-    <div className='HomeSearch'>
+    <div className='HomeSearch pt-5'>
       <div className="container">
         <div className="row">
-          <div className="col-md-8 offset-md-2"></div>
+          <div className="col-md-8 offset-md-2">
+            <InputGroup className="mb-3">
+              <Form.Control
+                placeholder="Поиск всего, что вы ищете…"
+                aria-label="Поиск всего, что вы ищете…"
+                aria-describedby="basic-addon2"
+              />
+              <Button variant="outline-secondary" id="button-addon2">
+                <FaSearch/>
+              </Button>
+            </InputGroup>
+          </div>
         </div>
+
       </div>
     </div>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomeHeader from '../components/Home/HomeHeader'
+import HomeSearch from '../components/Home/HomeSearch'
 
 export default function Home() {
   return (
     <div className='Home'>
       <HomeHeader/>
+      <HomeSearch/>
     </div>
   )
 }
