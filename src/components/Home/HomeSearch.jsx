aeaf63eb546a789c47e180b9d353bd2f16f0afcc
jamatov <mirabdullajamatov@gmail.com>
2023-01-05 11:42:50 +0500

    home search  start

---
+++ src/components/Home/HomeSearch.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function HomeSearch() {
+  return (
+    <div className='HomeSearch'>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 offset-md-2"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
