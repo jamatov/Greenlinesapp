dd19ccfd004ab8ed5ed40c620a18b4a3df9dfaa5
jamatov <mirabdullajamatov@gmail.com>
2023-01-09 16:00:54 +0500

    s

--- src/components/Home/HomeOtherLinks.jsx
+++ src/components/Home/HomeOtherLinks.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Img1 from '../../images/other-links-card1.jpeg'
 
 export default function HomeOtherLinks() {
   return (
@@ -13,6 +14,21 @@ export default function HomeOtherLinks() {
               </div>
               
               <h3>ИСТОРИИ ПАЦИЕНТОВ</h3>
+
+              <div className="card-body">
+                <div className="pt-item row pt-3 pb-4">
+                  <div className="col-lg-4">
+                    <img className='w-100' src={Img1} alt="" />
+                  </div>
+                  <div className="col-lg-8">
+                    <p>Поразительная история разделения близнецов в Acıbadem</p>
+                  </div>
+
+                  <div className="col text-right mr-3">
+                    <button className='btn btn-sm btn-light'>Читать далее</button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         
